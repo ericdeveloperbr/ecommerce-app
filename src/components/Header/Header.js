1a10e2b567c1ee3fa6491fe833b81899310fc98f
Eric Henrique <ericdeveloper@outlook.com>
2022-04-30 20:19:01 -0300

    fixing liting errors with our images

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import { FaShoppingCart } from 'react-icons/fa';
+
+import Container from '@components/Container';
+
+import styles from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <Container className={styles.headerContainer}>
+        <p className={styles.headerTitle}>Hyper Bros. Trading Cards</p>
+        <p className={styles.headerCard}>
+          <FaShoppingCart />
+          $0.00
+        </p>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
